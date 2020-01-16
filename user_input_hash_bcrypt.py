fdd3e052470ae1638bc0cc3550a868f702bfaa1c
Justin <56330379+JustinDo720@users.noreply.github.com>
2020-01-15 23:51:24 -0500

    Create user_input_hash_bcrypt.py

---
+++ user_input_hash_bcrypt.py
@@ -0,0 +1,28 @@
+"""My attempt with using the bcrypt module to encrypt user's input"""
+
+from bcrypt import hashpw, gensalt
+from time import sleep
+
+
+name = input('Please enter your Name:')
+user_name = input('Please enter your Username:')
+password = input('Please enter your Password:')
+
+sleep(3)
+
+print(f'''\nHello {name}, your original username and password are as follows:
+{user_name}
+{password}''')
+
+user_name = str.encode(user_name)
+password = str.encode(password)
+
+salt = gensalt()
+hash_user_name = hashpw(user_name,salt)
+hash_user_password = hashpw(password,salt)
+
+sleep(3)
+
+print(f'''\nYour hashed username and password are as:
+Username:{hash_user_name}
+Password: {hash_user_password}''')
